62a06ae93985e203a8f5a624bcf119796beb3aef
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-09-15 23:30:04 +0300

    [feat]:Make an e-learning task with angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,24 @@
 import { Component, signal } from '@angular/core';
+
+import { Header } from "./components/HeaderComponent/header";
+import { SidebarComponent } from './components/sidebar-component/sidebar-component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { C } from '@angular/cdk/keycodes';
+import { SubjectsComponent } from './components/subjects-component/subjects-component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header,
+    MatButtonModule,
+    MatIconModule,
+    SidebarComponent,
+    RouterOutlet,
+    CommonModule,
+    SubjectsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
