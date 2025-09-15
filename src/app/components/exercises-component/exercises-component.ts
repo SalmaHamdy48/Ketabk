377540a7fa8b7e2905400d93f553d42dc6007edf
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-09-16 01:45:35 +0300

    [fix]:fix drag and drop feature

--- src/app/components/exercises-component/exercises-component.ts
+++ src/app/components/exercises-component/exercises-component.ts
@@ -1,27 +1,33 @@
 import { Component } from '@angular/core';
+import { DragDropModule, CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';  // 👈 ده مهم
-import { DragDropModule, CdkDragStart } from '@angular/cdk/drag-drop';
-import { SingleChoiceCardComponent } from "../single-choice-card-component/single-choice-card-component";
-import { MultipleChoiceCardComponent } from "../multiple-choice-card-component/multiple-choice-card-component";
-import { TextCardComponent } from "../text-card-component/text-card-component";
+import { SingleChoiceCardComponent } from '../single-choice-card-component/single-choice-card-component';
+import { MultipleChoiceCardComponent } from '../multiple-choice-card-component/multiple-choice-card-component';
+import { TextCardComponent } from '../text-card-component/text-card-component';
 
 
 @Component({
   selector: 'app-exercises-component',
   standalone: true,
-  imports: [SingleChoiceCardComponent, MultipleChoiceCardComponent, TextCardComponent, DragDropModule, CommonModule, FormsModule],
+  imports: [
+    DragDropModule,
+    CommonModule,
+    SingleChoiceCardComponent,
+    MultipleChoiceCardComponent,
+    TextCardComponent
+  ],
   templateUrl: './exercises-component.html',
   styleUrl: './exercises-component.css'
 })
 export class ExercisesComponent {
   questionTypes = ['اختيار فردي', 'اختيار من متعدد', 'نص'];
-  droppedTypes: string[] = [];
+  droppedQuestions: string[] = [];
 
-  onDrop(type: string) {
-    // أضف النوع للمصفوفة
-    this.droppedTypes.push(type);
-    console.log('تم إدراج السؤال:', type);
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      const item = event.previousContainer.data[event.previousIndex];
+      this.droppedQuestions.push(item);
+    }
   }
 }
 
