cf76da183676995420c0a154416a11f9c0299a8c
Salma <salma.hamdy@appnest.pro>
2025-09-19 20:45:16 +0300

    initial commit

--- UniversitySystem/UniversitySystem/AppMetaData/BaseRouter/Router.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversitySystem.AppMetaData.BaseRouter
-{
-    public partial class Router
-    {
-        private const string root = "Api";
-        private const string version = "V1";
-        private const string Rule = root + "/" + version + "/";
-    }
-}
